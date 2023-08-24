f7215e41abbadf3284104a555f0e0d83c88ece88
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-08-24 23:15:06 +0300

    <BE> created the follow user api

--- backend/controllers/users.controllers.js
+++ backend/controllers/users.controllers.js
@@ -1,14 +1,33 @@
-const User = require("../models/users.model")
+const User = require("../models/users.model");
+const jwt = require("jsonwebtoken");
 
-const getAllUsers = async (req, res)=>{
-    const users = await User.find();
-    res.send(users)
-}
+const getAllUsers = async (req, res) => {
+	const users = await User.find();
+	res.send(users);
+};
 
-const getProfile = async (req, res)=>{
-    const user = await User.findById(req.user._id)
-    res.send(user)
-}
+const followUser = async (req, res) => {
+	console.log(req.body);
+	try {
+		const token = req.headers.authorization?.split(" ")[1];
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		const user_id = decoded._id;
+		const following_id = req.body.user_id;
+
+		const user = await User.findByIdAndUpdate(
+			user_id,
+			{ $addToSet: { following: following_id } },
+			{ new: true }
+		);
+
+		res.send(user);
+	} catch (error) {
+		console.error("Error following user:", error);
+		res.status(500).send({
+			message: "An error occurred while following user.",
+		});
+	}
+};
 
 
-module.exports = {getAllUsers, getProfile}
+module.exports = { getAllUsers, followUser, unfollowUser };
