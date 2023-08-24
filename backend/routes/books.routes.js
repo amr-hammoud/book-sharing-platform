0949d4601a64e0bad4ee1888a710891a0079efeb
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-08-24 23:15:42 +0300

    <BE> updated books routes

--- backend/routes/books.routes.js
+++ backend/routes/books.routes.js
@@ -5,5 +5,7 @@ const booksController = require("../controllers/books.controllers");
 router.get("/", booksController.getAllBooks)
 router.get("/:id", booksController.getBook)
 router.post("/create", booksController.createBook)
+router.post("/like", booksController.likeBook)
+router.post("/unlike", booksController.unlikeBook)
 
 module.exports = router;
