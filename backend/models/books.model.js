0c8638b8f6ed7fa7efce530f4a9549e9b40d48d7
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-08-24 23:13:30 +0300

    <BE> updated books model

--- backend/models/books.model.js
+++ backend/models/books.model.js
@@ -1,20 +1,24 @@
 const mongoose = require("mongoose");
 
-const booksSchema = new mongoose.Schema({
-    name: String,
-    author: String,
-    picture: String,
-    review: String,
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    },
-    likes: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    }
-}, { timestamps: true });
-
+const booksSchema = new mongoose.Schema(
+	{
+		name: String,
+		author: String,
+		picture: String,
+		review: String,
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+		},
+		likes: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "User",
+			},
+		],
+	},
+	{ timestamps: true }
+);
 
 const Book = mongoose.model("Book", booksSchema);
 module.exports = Book;
