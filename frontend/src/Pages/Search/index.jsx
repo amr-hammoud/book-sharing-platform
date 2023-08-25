a7963d8ebc239f4e29bb0237d5ef1ceb7bc1c46f
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-08-25 04:38:27 +0300

    <FE> created the search component

---
+++ frontend/src/Pages/Search/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "../../Components/Common/Navbar";
+import Input from "../../Components/Base/Input";
+
+const Search = () => {
+	return (
+		<div className="navbar-page light-bg">
+			<Navbar />
+			<div className="container white-bg create-book">
+				<div className="flex center color-primary mt-20">
+					<h1>Search Books</h1>
+				</div>
+				<Input />
+			</div>
+		</div>
+	);
+};
+
+export default Search;
